aa23ab9af8f047dbf93514b63966436594c029f9
VictoriaMakaryan <victoria.makaryan05@gmail.com>
2024-06-05 13:18:02 +0400

    new changes to my first file

--- file.cpp
+++ file.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
-	std::cout << "Hello";
+    // Seed the random number generator
+    srand(time(0));
+
+    // Generate and print a random number between 1 and 100
+    int randomNumber = rand() % 100 + 1;
+    std::cout << "Random number: " << randomNumber << std::endl;
+
+    return 0;
 }
+
